5daeefb46b3162d7469781673bd5d6ab5ed01a7d
m1chaeL <xiaocx0code@163.com>
2022-01-26 10:29:20 +0800

    az

--- Eathan/static/index.js
+++ Eathan/static/index.js
@@ -341,11 +341,11 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 30).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '叔叔很生气';
-    if (score <= 99) return '你所热爱的就是你的生活';
-    if (score <= 149) return '您就是蒙古上单？';
-    if (score <= 199) return '您马什么时候4啊？';
-    return '人？';
+    if (score <= 59) return '瀚泽表示不屑';
+    if (score <= 119) return '就这？';
+    if (score <= 159) return '瀚泽表示还行';
+    if (score <= 250) return '您就是瀚泽是吧？';
+    return '？瀚泽奶奶都吓哭了';
 }
 
 function toStr(obj) {
